1315c4efe1dd0f643bb4b612bcd91109d49a60bd
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-09-25 17:20:06 +0200

    Names app v1 ended

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 // src/index.ts
 function displayName(name: string | null | undefined): void {
-    if (name == null || name == undefined) name = "";
+    if (name == null || name == undefined || name == "") name = "Anonymous";
     const card = document.createElement("ion-card");
     const cardHeader = document.createElement("ion-card-header");
     const cardTitle = document.createElement("ion-card-title");
     cardTitle.textContent = name;
     cardHeader.appendChild(cardTitle);
     card.appendChild(cardHeader);
-    const wrapper = document.getElementById("wrapper");
-    if (wrapper) wrapper.append(card);
+    const names = document.getElementById("names");
+    if (names) names.append(card);
 }
 
-document.getElementById("ionic-button")?.addEventListener('click', function() {
+function addName() {
     const ionInput = (document.getElementById("ionic-input") as HTMLInputElement);
     displayName(ionInput.value);
     ionInput.value = "";
-});
+}
+
+document.getElementById("ionic-button")?.addEventListener('click', addName);
+//document.getElementById("ionic-button")?.addEventListener('keydown', (event: KeyboardEvent) => {
+//    console.log(event.key);
+//    if (event.key === 'Enter') addName();
+//})
